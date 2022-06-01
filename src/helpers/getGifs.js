0795b752e5d382b60555f8ddc92f22df8bb7bc59
victor-aguilar <74022888+vidi-server@users.noreply.github.com>
2022-06-01 17:51:49 -0500

    https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) => {
-    const url = `http://api.giphy.com/v1/stickers/search?q=${encodeURI(category)}&limit=16&api_key=M4hdJd9sNEUz7yPXh428ygoWNEemRvpB`
+    const url = `https://api.giphy.com/v1/stickers/search?q=${encodeURI(category)}&limit=16&api_key=M4hdJd9sNEUz7yPXh428ygoWNEemRvpB`
 
     const resp = await fetch(url);
     const {data} = await resp.json();
